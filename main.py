1ea8b49e14b9526ce08371d8527a6ee28848ceb8
LianaSl <sljonkina@gmail.com>
2026-06-21 19:42:25 +0200

    изменения

---
+++ main.py
@@ -0,0 +1,20 @@
+contacts = []
+
+def add_contact(name, phone, email):
+    contact = {"name": name, "phone": phone, "email": email}  # ← добавить отступ
+    contacts.append(contact)
+
+def show_all():
+    for contact in contacts:
+        print(f"Имя: {contact['name']}, Телефон: {contact['phone']}, Email: {contact['email']}")
+
+def find_contact(query):
+    result = []
+    for contact in contacts:
+        if query == contact["name"] or query == contact["phone"]:
+            result.append(contact)
+    return result
+
+add_contact("Dinara", "+79174465420", "bikdifa@mail.ru")
+for contact in contacts:
+    print(contact)
